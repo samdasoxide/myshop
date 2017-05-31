156eaebcb838129c681968945875656fbb32da06
samdasoxide <samdasoxide@gmail.com>
2017-05-31 17:24:27 +0300

    first commit

---
+++ shop/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Category
+    '''
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Product
+    '''
+    list_display = ('name', 'slug', 'price', 'stock',
+                    'available', 'created', 'updated')
+    list_filter = ('available', 'created', 'updated')
+    list_editable = ('price', 'stock', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
